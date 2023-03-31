732f2ac3e9c9664f03dc3e522bea5dedf026f182
cbrane <connor@connorraney.com>
2023-03-31 12:00:59 -0400

    changed setup.sh to add the theme info in it

--- setup.sh
+++ setup.sh
@@ -5,7 +5,13 @@ echo "\
 email = \"your-email@domain.com\"\n\
 " > ~/.streamlit/credentials.toml
 
-echo "[server]
+echo "[theme]
+primaryColor = '#7e8f89'
+backgroundColor ='#1c231e'
+secondaryBackgroundColor = '#6d7871'
+textColor = '#e7e8e1'
+font = 'monospace'
+[server]
 headless = true
 port = $PORT
 enableCORS = false
